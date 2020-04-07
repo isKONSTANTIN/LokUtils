d6b2c0c824b6f96623f1db69f1d2c24d248261c5
lokin135 <lokin135help@gmail.com>
2020-04-07 20:24:02 +0300

    UICanvas: added getObject() and getInputs() methods, inputs field

--- UI/ru/lokincompany/lokutils/ui/objects/UICanvas.java
+++ UI/ru/lokincompany/lokutils/ui/objects/UICanvas.java
@@ -1,6 +1,8 @@
 package ru.lokincompany.lokutils.ui.objects;
 
 import org.lwjgl.util.vector.Vector2f;
+import ru.lokincompany.lokutils.input.Inputs;
+import ru.lokincompany.lokutils.render.GLContext;
 import ru.lokincompany.lokutils.render.RenderPart;
 import ru.lokincompany.lokutils.render.tools.ViewTools;
 import ru.lokincompany.lokutils.ui.UIObject;
@@ -14,12 +16,31 @@ public class UICanvas extends UIObject {
     protected Vector<UIObject> objects = new Vector<>();
     protected Vector<RenderPart> renderParts = new Vector<>();
     protected UICanvasRender render;
+    protected Inputs inputs;
 
     protected final Object updateSync = new Object();
 
-    public UICanvas(){
+    public UICanvas(Inputs inputs){
+        this.inputs = inputs;
         render = new UICanvasRender(this);
         setSize(new Vector2f(256, 256));
+    }
+
+    public UICanvas(){
+        this(GLContext.getCurrent().getWindow().getInputs());
+    }
+
+    public Inputs getInputs() {
+        return inputs;
+    }
+
+    public UIObject getObject(String name){
+        for (UIObject object : objects){
+            if (object.getName().equals(name))
+                return object;
+        }
+
+        return null;
     }
 
     public UICanvas addObject(UIObject object){
@@ -42,7 +63,7 @@ public class UICanvas extends UIObject {
 
     @Override
     public RenderPart update(UICanvas parent) {
-        super.update(parent);
+        super.update(parent != null ? parent : this);
 
         ArrayList<RenderPart> rp = new ArrayList<>();
 
