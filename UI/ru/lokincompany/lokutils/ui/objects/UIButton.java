6d90ec534470be258edcd68ecf935b344b70a3f9
lokin135 <kostya1661@gmail.com>
2020-04-10 16:33:37 +0300

    UIButton: added render

--- UI/ru/lokincompany/lokutils/ui/objects/UIButton.java
+++ UI/ru/lokincompany/lokutils/ui/objects/UIButton.java
@@ -1,6 +1,35 @@
 package ru.lokincompany.lokutils.ui.objects;
 
+import org.lwjgl.util.vector.Vector2f;
 import ru.lokincompany.lokutils.ui.UIObject;
+import ru.lokincompany.lokutils.ui.positioning.Position;
+import ru.lokincompany.lokutils.ui.positioning.PositioningSetter;
 
 public class UIButton extends UIObject {
+
+    UIText text;
+    UIPanel panel;
+
+    public UIButton() {
+        this.panel = (UIPanel) new UIPanel().setPosition(new PositioningSetter(this::getPosition)).setSize(new PositioningSetter(this::getSize));
+        this.text = (UIText) new UIText().setText("Button").setPosition(new PositioningSetter(Position.Center));
+        panel.getCanvas().addObject(text);
+
+        setSize(new Vector2f(100,30));
+    }
+
+    public UIText getText() {
+        return text;
+    }
+
+    public UIPanel getPanel() {
+        return panel;
+    }
+
+    @Override
+    public void update(UIObject parent) {
+        super.update(parent);
+
+        panel.update(this);
+    }
 }
