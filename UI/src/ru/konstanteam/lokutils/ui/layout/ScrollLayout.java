2cd69e0936bd10d5f6ce0b9b6b3fe511bcd699a8
isKONSTANTIN <lokin135@mail.lokincompany.ru>
2020-08-19 13:16:23 +0300

    New layout: scroll

---
+++ UI/src/ru/konstanteam/lokutils/ui/layout/ScrollLayout.java
@@ -0,0 +1,80 @@
+package ru.konstanteam.lokutils.ui.layout;
+
+import ru.konstanteam.lokutils.objects.Point;
+import ru.konstanteam.lokutils.objects.Rect;
+import ru.konstanteam.lokutils.objects.Size;
+import ru.konstanteam.lokutils.render.GLContext;
+import ru.konstanteam.lokutils.render.tools.ViewTools;
+import ru.konstanteam.lokutils.ui.UIObject;
+import ru.konstanteam.lokutils.ui.eventsystem.Event;
+import ru.konstanteam.lokutils.ui.eventsystem.EventCustomer;
+import ru.konstanteam.lokutils.ui.eventsystem.events.MouseScrollEvent;
+
+public class ScrollLayout extends FreeLayout {
+    protected float scroll;
+    protected float scrollMomentum;
+    protected float momentumFactor = 0.8f;
+    protected float scrollFactor = 2f;
+
+    public ScrollLayout() {
+        customersContainer.addCustomer(event -> {
+            if (focusedObject == null && lastParent == null)
+                return;
+
+            UIObject focusedObjectAtOwner = lastParent != null ? lastParent.getOwner().focusedObject : null;
+
+            if (focusedObjectAtOwner == null || !focusedObjectAtOwner.equals(this))
+                return;
+
+            scrollMomentum += event.scrollDelta.y;
+        }, MouseScrollEvent.class);
+    }
+
+    @Override
+    protected Point getObjectPos(UIObject object) {
+        return super.getObjectPos(object).offset(0, scroll);
+    }
+
+    @Override
+    protected Point getLazyObjectPos(UIObject object) {
+        return super.getLazyObjectPos(object).offset(0, scroll);
+    }
+
+    public float getMomentumFactor() {
+        return momentumFactor;
+    }
+
+    public void setMomentumFactor(float momentumFactor) {
+        this.momentumFactor = momentumFactor;
+    }
+
+    public float getScrollFactor() {
+        return scrollFactor;
+    }
+
+    public void setScrollFactor(float scrollFactor) {
+        this.scrollFactor = scrollFactor;
+    }
+
+    @Override
+    public void render() {
+        scroll += scrollMomentum * scrollFactor;
+        scrollMomentum *= momentumFactor;
+
+        ViewTools viewTools = GLContext.getCurrent().getViewTools();
+        Size mySize = size().get();
+
+        viewTools.pushLook(new Rect(0, 0, mySize.width, mySize.height), 0);
+
+        for (UIObject object : objects) {
+            Point objectPosition = getObjectPos(object);
+            Size objectSize = object.size().get();
+
+            viewTools.pushLook(new Rect(objectPosition.x, objectPosition.y, objectSize.width, objectSize.height));
+            object.render();
+            viewTools.popLook();
+        }
+
+        viewTools.popLook();
+    }
+}
--- src/ru/konstanteam/lokutils/Main.java
+++ src/ru/konstanteam/lokutils/Main.java
@@ -1,6 +1,7 @@
 package ru.konstanteam.lokutils;
 
 import ru.konstanteam.lokutils.applications.Application;
+import ru.konstanteam.lokutils.objects.Point;
 import ru.konstanteam.lokutils.render.GLFW;
 import ru.konstanteam.lokutils.render.Window;
 import ru.konstanteam.lokutils.ui.core.maincanvas.UIMainCanvasSystem;
@@ -8,20 +9,20 @@ import ru.konstanteam.lokutils.ui.core.windows.UIWindowSystem;
 import ru.konstanteam.lokutils.ui.layout.Alignment;
 import ru.konstanteam.lokutils.ui.layout.FreeLayout;
 import ru.konstanteam.lokutils.ui.layout.ListLayout;
+import ru.konstanteam.lokutils.ui.layout.ScrollLayout;
+import ru.konstanteam.lokutils.ui.objects.UIBlackout;
 import ru.konstanteam.lokutils.ui.objects.UITextField;
 
 import java.util.Random;
 
-public class Main extends Application<UIMainCanvasSystem<ListLayout>> {
+public class Main extends Application<UIMainCanvasSystem<FreeLayout>> {
     public Main() {
         super(
-                new UIMainCanvasSystem<>(new ListLayout())
+                new UIMainCanvasSystem<>(new FreeLayout())
         );
     }
 
     public static void main(String[] args) {
-        GLFW.init();
-
         new Main().open();
     }
 
@@ -29,8 +30,14 @@ public class Main extends Application<UIMainCanvasSystem<ListLayout>> {
     public void initEvent() {
         window.setWindowCloseCallback((window) -> this.close());
         UITextField field = new UITextField().setEnterAction(() -> System.out.println(new Random().nextInt()));
+        ScrollLayout layout = new ScrollLayout();
 
-        uiController.getLayout().addObject(field);
+        layout.addObject(field, Alignment.CENTER);
+        UIBlackout blackout = new UIBlackout();
+        blackout.size().set(layout.size());
+
+        uiController.getLayout().addObject(blackout, Alignment.CENTER);
+        uiController.getLayout().addObject(layout, Alignment.CENTER);
     }
 
 }
